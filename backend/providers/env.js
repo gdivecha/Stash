a42d5a0c62a1f0ae870ee5a458bf3be78834913a
Gaurav Divecha <gauravcdivecha@gmail.com>
2026-09-11 21:46:43 -0400

    refactor: restructure project directory into backend and client modules

--- backend/providers/env.js
+++ backend/providers/env.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 config({
     path: path.join(
         __dirname, 
-        '../', 
+        '../../', 
         `.env.${process.env.NODE_ENV || 'development'}.local`
     )
 });
